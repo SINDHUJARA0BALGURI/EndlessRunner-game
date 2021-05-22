96adb13a541643919c931df75f4477da6b403652
SindhujaBalguri <sindhuja.balguri@xcubelabs.com>
2021-05-22 18:35:49 +0530

    This is the practise project of Endlessrunnergame

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private AudioSource playerAudio;
+    private Animator playerAnim;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isGround = true;
+    public bool gameOver = false;
+    public AudioClip jumpSound;
+    public AudioClip clashSound;
+    public ParticleSystem explosionParticle;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+        
+       //rb.AddForce(Vector3.up * 1000);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&isGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+   
+            isGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+       
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+       
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("GAME OVER");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(clashSound, 1.0f);
+        }
+    }
+}
